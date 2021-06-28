317330c50563403c9c53f704b89e94cd1dc21131
mirzokhidkhasanov <mirzohidkhasanov12@gmail.com>
2021-06-28 15:40:48 +0500

    smth changed

--- src/main/java/uz/mk/apponlineshop/config/SecurityConfig.java
+++ src/main/java/uz/mk/apponlineshop/config/SecurityConfig.java
@@ -16,14 +16,6 @@ import uz.mk.apponlineshop.model.Role;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    1-vazifa
-//
-//    https://pcmarket.uz saytining security qismini yozish. Bunda inMemory da 3 ta user qo'shiladi.
-//    SUPER_ADMIN, MODERATOR va OPERATOR rollari bo’lsin.
-//    SUPER_ADMIN har qanday ishni qila oladi;
-//    MODERATOR mahsulot qo’sha oladi va tahrirlay oladi ,ammo o’chira olmaydi;
-//    OPERATOR buyurtmalar bilan ishlaydi, mahsulotni o'chira olmaydi va tahrirlay olmaydi.
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
